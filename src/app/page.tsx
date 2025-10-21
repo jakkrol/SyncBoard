72c35e6aa46e480d4f1c8e399ef4e5eb2d781723
jakrol <kozak10555@gmail.com>
2025-10-21 22:32:59 +0200

    rooms

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -6,16 +6,21 @@ export default function RoomSelect() {
   const [room, setRoom] = useState("");
   const router = useRouter();
 
-  const joinRoom = () => {
-    if (!room) return;
-    router.push(`/canvas/${room}`);
+  const joinRoom = (r?: string) => {
+    const roomToJoin = r || room;
+    if (!roomToJoin) return;
+    router.push(`/canvas/${roomToJoin}`);
   };
 
   return (
     <div>
       <h1 className="m-1">Choose a room</h1>
       <input className="m-1" value={room} onChange={e => setRoom(e.target.value)} placeholder="Room name" />
-      <button onClick={joinRoom}>Join</button>
+      <button onClick={() => joinRoom()}>Join</button>
+      <label className="m-1" htmlFor="roomInput" onClick={() => joinRoom("room1")}>Room1</label>
+      <label className="m-1" htmlFor="roomInput" onClick={() => joinRoom("room2")}>Room2</label>
+      <label className="m-1" htmlFor="roomInput" onClick={() => joinRoom("room3")}>Room3</label>
+      <label className="m-1" htmlFor="roomInput" onClick={() => joinRoom("room4")}>Room4</label>
     </div>
   );
 }
