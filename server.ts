81e3a30d821e3cb158ddba33f5bbfb8478eccd84
jakrol <kozak10555@gmail.com>
2025-10-23 19:34:34 +0200

    .

--- server.ts
+++ server.ts
@@ -19,12 +19,26 @@ app.prepare().then(() => {
     }
   });
 
+  const boards: { [room: string]: string } = {};
+
   io.on("connection", (socket: any) => {
     console.log(`a user connected: ${socket.id}`);
     socket.on("join", (room: string) => {
       socket.join(room);
       console.log(`socket ${socket.id} joined room ${room}`);
+      if(boards[room]) {
+        socket.emit("loadBoard", boards[room]);
+      }
     })
+
+
+
+    socket.on("saveBoard", ({room, data}: {room: string, data: string}) => {
+      boards[room] = data;
+      console.log(`Board saved for room ${room}`);
+    });
+
+    
 
 
     socket.on("moveCard", (data: any) => {
