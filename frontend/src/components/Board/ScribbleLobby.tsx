8f49f1124412cc3d896c57145a200e117168f7af
jakrol <kozak10555@gmail.com>
2026-07-25 13:57:34 +0200

    update package-lock.json and refactor ScribbleLobby component styles

--- frontend/src/components/Board/ScribbleLobby.tsx
+++ frontend/src/components/Board/ScribbleLobby.tsx
@@ -14,12 +14,12 @@ export default function ScribbleLobby({ socket, room, players, onStart }: Scribb
     // const [socket, setSocket] = useState<Socket | null>(null);
 
     return (
-        <div className="flex justify-center items-center h-screen w-full bg-gradient-to-br from-gray-900 to-black">
+        <div className="flex justify-center items-center h-screen w-full bg-linear-to-br from-gray-900 to-black">
             <div className="w-full max-w-md items-center justify-center flex flex-col gap-4 rounded-2xl border-6 border-gray-900 p-6 shadow-xl">
                 <div>
                     Connected players: {players.length}
                 </div>
-                <button className="bg-gray-500 hover:bg-gray-700 text-white font-bold py-2 px-4 border border-gray-700 rounded"onClick={onStart} disabled={players.length < 2}>Start game</button>
+                <button className="bg-gray-500 hover:bg-gray-700 text-white font-bold py-2 px-4 border border-gray-700 rounded" onClick={onStart} disabled={players.length < 2}>Start game</button>
             </div>
         </div>
     );
